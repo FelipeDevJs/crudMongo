b24a14b4622ec781d84cc6f70c37388241d259d2
FelipeViana <sr.felipe.viana@gmail.com>
2023-03-15 12:10:29 +0000

    conexao atualizado

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,21 @@
-const mongoose = require('mongoose')
-require('dotenv').config()
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect(
-    process.env.DB_URL,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
-  );
+if (!process.env.DB_URL) {
+  console.error('A variável de ambiente DB_URL não está definida.');
+  process.exit(1);
+}
+
+mongoose.set('strictQuery', false);
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(process.env.DB_URL, options);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Erro na conexão:'));
+db.once('open', () => {
+  console.log('Conexão bem-sucedida!');
+});
